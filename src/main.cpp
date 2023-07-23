840a379791137c268a8e4f89e3cb2abd4e4f64d1
talksik <patel.arjun50@gmail.com>
2023-07-23 06:53:57 -0700

    adding in simple button

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+#include "gtkmm/button.h"
 #include <gtkmm.h>
+#include <iostream>
 
 class MyWindow : public Gtk::Window {
 public:
@@ -7,7 +9,14 @@ public:
 
 MyWindow::MyWindow() {
   set_title("Basic application");
+
+  Gtk::Button button("Hello World");
+  button.signal_clicked().connect([] { std::cout << "Hello World\n"; });
+  button.set_label("Hello World");
+
   set_default_size(800, 450);
+
+  set_child(button);
 }
 
 int main(int argc, char **argv) {
